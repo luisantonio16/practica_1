90fe233bb3f30c5c823e2840d2a7f34c670a0346
Luis Mercedes melenciano <luis16.16m@gmail.com>
2020-09-10 09:50:26 -0500

    mayor_y_menor

---
+++ mayor_y_menor.py
@@ -0,0 +1,8 @@
+numero1 = int(input("Elija un Numero: "))
+numero2 = int(input("Digite otro Numero: "))
+
+if numero1 > numero2:
+    print("EL " + str(numero1)+" es mayor que " +str(numero2))
+
+elif numero2 > numero1:
+     print("EL "+ str(numero2) +" es mayor que " + str(numero1))
